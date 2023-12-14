d06560822f8e159fa37746184e292e4abda6f8cd
liuxi <liuxi@sinomartin.com>
2023-12-14 14:12:52 +0800

    上下文性能优化方案-useMemo

--- src/03-useMemo/App.jsx
+++ src/03-useMemo/App.jsx
@@ -42,15 +42,3 @@ export default function Page({
         </>
     );
 }
-
-// 父组件重渲染，子组件一定会重渲染 —— 即使子组件不接受props
-// 官方提供 memo api 帮助组件在接受的 props 不改变的情况下跳过渲染
-
-// 性能优化
-// 子组件使用
-// ! 一.不推荐
-//  1. props 中对象用 useMemo 包裹返回
-//  2. props 中函数用 useCallback 包裹返回
-//  3. 子组件定义时使用 memo api
-// ? 二.推荐✔
-//  1. 在父组件中使用 useMemo 包裹需要使用的子组件返回一个变量，在 return jsx 中使用
